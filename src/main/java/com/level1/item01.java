7584f77e36e5bf273756df43d276d200f2243db3
kms <kkang@gangminseong-ui-MacBookPro.local>
2021-12-13 23:00:25 +0900

    test

---
+++ src/main/java/com/level1/item01.java
@@ -0,0 +1,9 @@
+package com.level1;
+
+public class item01 {
+    String name;
+
+    public static void main(String[] args) {
+        // 생성자 대신 정적 팩터리 메서드를 사용할 수 없는지 생각..
+    }
+}
---
+++ src/test/java/com/level1/EffectiveStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.level1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
